25da91b1b3bc83996ddd788e1e19079eef53fff5
paula <paula@Blues>
2022-12-02 00:25:41 +1100

    removing undo tree

---
+++ target/rls/debug/deps/AOC-41772d5503223f2b.d
@@ -0,0 +1,7 @@
+/home/paula/AdventOfCode2022/target/rls/debug/deps/AOC-41772d5503223f2b.rmeta: src/main.rs src/days/mod.rs src/days/day_1.rs
+
+/home/paula/AdventOfCode2022/target/rls/debug/deps/AOC-41772d5503223f2b.d: src/main.rs src/days/mod.rs src/days/day_1.rs
+
+src/main.rs:
+src/days/mod.rs:
+src/days/day_1.rs:
